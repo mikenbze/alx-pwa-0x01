da6080baec6ad78a239c649d522ce2763a6389ac
Michael Muriungi <50227674+mikenbze@users.noreply.github.com>
2025-08-22 19:13:05 +0300

    Update next.config.ts

--- alx-movie-app/next.config.ts
+++ alx-movie-app/next.config.ts
@@ -1,8 +1,20 @@
 import type { NextConfig } from "next";
+import withPWAInit from "@ducanh2912/next-pwa";
+
+// Initialize next-pwa with options
+const withPWA = withPWAInit({
+  dest: "public", // where the service worker + manifest will be generated
+});
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactStrictMode: true,
+  images: {
+    domains: ["m.media-amazon.com"], // add more domains as needed
+  },
 };
 
-export default nextConfig;
+// Export combined config
+export default withPWA({
+  ...nextConfig,
+});
+
