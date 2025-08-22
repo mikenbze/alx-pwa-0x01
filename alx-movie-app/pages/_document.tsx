4fe27e798976d55cef18025e325de418a7281186
Michael Muriungi <50227674+mikenbze@users.noreply.github.com>
2025-08-22 20:36:52 +0300

    Update manifest.json

--- alx-movie-app/pages/_document.tsx
+++ alx-movie-app/pages/_document.tsx
@@ -1,9 +1,27 @@
+// pages/_document.tsx
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        {/* Link the manifest for PWA */}
+        <link rel="manifest" href="/manifest.json" />
+
+        {/* Theme color for Chrome, Android */}
+        <meta name="theme-color" content="#FFFFFF" />
+
+        {/* Apple touch icon */}
+        <link
+          rel="apple-touch-icon"
+          sizes="152x152"
+          href="/icons/apple-icon-152x152.png"
+        />
+
+        {/* Microsoft tile */}
+        <meta name="msapplication-TileImage" content="/icons/ms-icon-310x310.png" />
+        <meta name="msapplication-TileColor" content="#FFFFFF" />
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
